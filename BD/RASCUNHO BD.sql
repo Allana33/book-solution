2f2b536af9b9b6b5098fb6e672b61ccf799c85bc
Allana33 <allana.alves08@hotmail.com>
2021-12-13 22:02:28 -0300

    continuação do banco

--- BD/RASCUNHO BD.sql
+++ BD/RASCUNHO BD.sql
@@ -20,14 +20,14 @@ id_empresa int primary key auto_increment not null,
 cnpj char(14) not null, 
 nome_empresa varchar(99) not null,
 telefone char(14) not null,
-email varchar(30) not null,  
+email varchar(30) not null,
+senha char(10)  
 );
 
 create table livro (
-id_livro int primary key auto_increment not null,
+id_livro int primary key auto_increment,
 titulo vachar(100) not null, autor vachar(30) not null, 
 data_publicacao date not null,
-genero varchar(30) not null, /* Genero seria muitos para muitos, então uma table somente para genero etc.. e fariamos uma relação */
 idioma varchar(30) not null,
 volume int, 
 edicao int,
@@ -38,26 +38,28 @@ descricao varchar(500),
 status enum('d','i') not null
 ); 
 
+create table autor(id_autor int primary key auto_increment, 
+nome_autor varchar(50) not null,
+nacionalidade varchar(40) not null
+);
+
 create table editora (
-id_editora int primary key auto_increment not null,
-nome_editora varchar(30) not null/*nacionalidade ou contato como opção*/
+id_editora int primary key auto_increment,
+nome_editora varchar(30) not null
+);
+
+create table genero( /* Genero seria muitos para muitos, então uma table somente para genero etc.. e fariamos uma relação */
+id_genero primary key auto_increment, 
+nome_genero varchar(30) not null,  
 );
 
 create table reserva (
-id_reserva int primary key auto_increment not null,
+id_reserva int primary key auto_increment,
 data_reserva date not null,
 prazo_reserva date not null,
-situacao_reserva enum('x','x') not null, 
-/*ponto a ser discutido na reunião*/id_livro int, 
-constraint fk_idlivro foreign key (id_livro) references livro (id_livro) /*vai ter chave estrangeira para fazer ligação de usuario?*/
-);
-
-create table agendamento (
-id_agendamento int primary key auto_increment not null,
-data_retirada date, 
-data_entrega date,
-id_livro int,
-constraint fk_idlivro foreign key (id_livro) references livro (id_livro) /*vai ter chave estrangeira para fazer ligação de usuario?*/
+situacao_reserva enum('d','i') not null, 
+id_cliente int,
+foreign key (id_cliente) references cliente (id_livro)
 );
 
 create table usuario (
@@ -67,4 +69,25 @@ nivel_acesso enum('a','u') not null,
 situacao_usuario enum('b','d') not null
 );
 
+Create table cliente(
+nome varchar(30) not null,
+sobrenome varchar(30) not null,
+cpf char(11) not null,
+sexo enum('m', 'f')
+data_cadastro date not null
+);
 
+create table genero_livro( --tabela relaçao
+genero_id_livro int primary key auto_increment,
+id_livro int, 
+foreign key (id_livro) references livro (id_livro)
+id_genero int,
+foreign key (id_genero) references (id_genero) 
+);
+
+ create table reserva_livro( --tabela relaçao
+livro_id_reserva int primary key auto_increment
+);
+ create table autor_livro( --tabela relaçao
+autor_id_livro int primary key auto_increment
+ ); 
