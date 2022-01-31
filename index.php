c1f7b7e6b8b0dfdfee8bf6b120631f13a71dd03c
lucasmenchon <it.lucas@outlook.com>
2022-01-31 11:44:52 -0300

    lottie adicionado ao bg, alguns bugs para fixar.

--- index.php
+++ index.php
@@ -12,28 +12,56 @@ include_once 'login.php';
     <title>Login</title>
 </head>
 
+
+
+
 <body>
+    <div class="lottie-bg">
+
+        <lottie-interactive path="assets/animations/bg.json"  background="transparent" speed="0.5" loop  autoplay style="left:0; right:0; position: absolute;">
+
+    </div>
+
+
+
+
+
+
+             
+            
+          
+
+             
+        
+            <div class="box-login">
+            <h1>Login</h1> 
+
+                <form method="POST" action="">
+
+                   <label>Usuário</label>
+                      <input type="text" name="usuario" placeholder="Usuário" value="<?php if(isset($dados['usuario'])){ echo $dados['usuario']; } ?>"><br><br>
+
+                    <label>Senha</label>
+
+                      <input type="password" name="senha_usuario" placeholder="Senha" value="<?php if(isset($dados['senha_usuario'])){ echo $dados['senha_usuario']; } ?>"><br><br>
+
+                      <input type="submit" value="Acessar" name="Login">
+
+                </form>
+                
+</div>
+   
+     
+        
     
-    <h1>Login</h1>
-
-    <?php
 
 
-    ?>
+    
+        <!--  Custom Scripts       -->
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<script type="text/javascript" src="https://unpkg.com/lottie-interactive@latest/dist/lottie-interactive.js"></script>
 
-    <form method="POST" action="">
-        <label>Usuário</label>
-        <input type="text" name="usuario" placeholder="Usuário" value="<?php if(isset($dados['usuario'])){ echo $dados['usuario']; } ?>"><br><br>
 
-        <label>Senha</label>
-        <input type="password" name="senha_usuario" placeholder="Senha" value="<?php if(isset($dados['senha_usuario'])){ echo $dados['senha_usuario']; } ?>"><br><br>
-
-        <input type="submit" value="Acessar" name="Login">
-    </form>
-
-    <br><br>
-    Usuário: thor<br>
-    Senha: 1234
 </body>
 
 </html>
