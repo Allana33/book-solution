c54e58cc563a5e98771e7fad70fae4823fa83204
lucasmenchon <it.lucas@outlook.com>
2022-02-14 17:51:03 -0300

    bug server mariadb, oracle opcao sair. tentando solucao

--- database/login.sql
+++ database/login.sql
@@ -1,3 +1,3 @@
 USE athena;
 
-INSERT INTO usuarios (email_usuario, senha_usuario, situacao_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$T0KXHYcO3GmWklS/0BqSzuOKtERZZI3hYNCojtLFOlYgoxb0LwGzq','d','admin');
+INSERT INTO usuarios (email_usuario, senha_usuario, status_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$YW/5bg0UERTwhHrqtFt//OGQA3lqTJ/BYRBq8N8nWEehF/QebVCaK','d','adminroot');
