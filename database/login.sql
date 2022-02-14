0ce63cf6379698dcf739ecbc3dfd0a1d3274e76f
lucasmenchon <it.lucas@outlook.com>
2022-02-14 17:46:10 -0300

    ...

--- database/login.sql
+++ database/login.sql
@@ -1,3 +1,3 @@
 USE athena;
 
-INSERT INTO usuarios (email_usuario, senha_usuario, situacao_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$Cr5WeQ0j2m3PvuLKXDBtCetoZe1LyBdjymeGHNsUNHsB6EpZDpivG','d','admin');
+INSERT INTO usuarios (email_usuario, senha_usuario, situacao_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$T0KXHYcO3GmWklS/0BqSzuOKtERZZI3hYNCojtLFOlYgoxb0LwGzq','d','admin');
