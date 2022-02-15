05ba55d145d6490c9eea0a03e0d9bce1b429bc4f
lucasmenchon <it.lucas@outlook.com>
2022-02-15 03:29:53 -0300

    att login.sql para hash de senha.

--- database/login.sql
+++ database/login.sql
@@ -1,3 +1,3 @@
 USE athena;
 
-INSERT INTO usuarios (email_usuario, senha_usuario, status_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$YW/5bg0UERTwhHrqtFt//OGQA3lqTJ/BYRBq8N8nWEehF/QebVCaK','d','adminroot');
+INSERT INTO usuarios (email_usuario, senha_usuario, status_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$YW/5bg0UERTwhHrqtFt//OGQA3lqTJ/BYRBq8N8nWEehF/QebVCaK','d','thoradmin');
