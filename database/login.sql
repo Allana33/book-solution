a273fbbfdb21534c57101a85854b566fe80c5820
lucasmenchon <it.lucas@outlook.com>
2022-02-15 03:32:07 -0300

    senha hash feita para acesso online.

--- database/login.sql
+++ database/login.sql
@@ -1,3 +1,3 @@
 USE athena;
 
-INSERT INTO usuarios (email_usuario, senha_usuario, status_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$YW/5bg0UERTwhHrqtFt//OGQA3lqTJ/BYRBq8N8nWEehF/QebVCaK','d','thoradmin');
+INSERT INTO usuarios (email_usuario, senha_usuario, status_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$vCfAeXzRj8IGtUk8M8B6pOTisGtl/bI/lDCR13C.qMl.eOyyFL0MK','d','thoradmin');
