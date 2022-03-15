88ef78dbeb187d9bf5ef5f03f529bf99512a39c0
lucasmenchon <it.lucas@outlook.com>
2022-03-15 09:26:52 -0300

    alteracao para teste em vm, implatando login

---
+++ .bd/login - vm.sql
@@ -0,0 +1,3 @@
+USE book_solution;
+
+INSERT INTO usuarios (email_usuario, senha_usuario, status_usuario, usuario) VALUES ('admin@admin.com', '$2y$10$ZVIbMxoB3eYm9aKzZ2mUUu2Opmj36CEC11LXleSgq6l3ZvzPEaHg6','d','thoradmin');
