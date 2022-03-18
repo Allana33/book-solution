60b3e804d54f5e1dda93b31a5ddbe8646729f770
unknown <allana.alves08@hotmail.com>
2022-03-17 22:13:51 -0300

    navbar att2

--- front-end/menu.php
+++ front-end/menu.php
@@ -1,22 +1,29 @@
 <header>
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #483D8B;">
         <div class="container-fluid">
-          <a class="navbar-brand text-white" .text-white href="#">Book Solution Logo</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <a class="navbar-brand text-white" .text-white href="#">  
+            <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+      <img src="assets/img/faviconx.png" alt="" width="40" height="40" class="d-inline-block align-text-top  justify-content-end">
+      Bootstrap
+    </a>
+    <button class="navbar-toggler  justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+  </div>
+    </a>
           <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav teste">
               <li class="nav-item teste">
-                <a class="nav-link active teste text-white" .text-white aria-current="page" href="#">Home</a>
+                <a class="nav-link active teste text-white" .text-white aria-current="page" href="home">Home</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown text-white" .text-white href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Cadastros
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a class="dropdown-item text-black" .text-black href="#">Usuários</a></li>
-                  <li><a class="dropdown-item text-black" .text-black href="#">Clientes</a></li>
+                  <li><a class="dropdown-item text-black" .text-black href="cadastro_usuario">Usuários</a></li>
+                  <li><a class="dropdown-item text-black" .text-black href="cliente_conteudo">Clientes</a></li>
                 </ul>
               </li>
               <li class="nav-item dropdown">
@@ -25,7 +32,7 @@
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                   <li><a class="dropdown-item" href="#">Consulta</a></li>
-                  <li><a class="dropdown-item" href="#">Cadastro</a></li>
+                  <li><a class="dropdown-item" href="cadastro_livro">Cadastro</a></li>
                 </ul>
               </li>
               <li class="nav-item dropdown">
