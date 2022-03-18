c94c17f3b9fe45bd5acc11b678b8b70efb6e1bcd
allana33 <allana.alves08@hotmail.com>
2022-03-17 21:36:23 -0300

    navbar att

--- front-end/cabecalho.php
+++ front-end/cabecalho.php
@@ -29,6 +29,7 @@ if( !isset($_SESSION['id']) OR !isset($_SESSION['usuario'])){
 <link rel="stylesheet" href="assets/css/reset.css">
 <link rel="stylesheet" href="assets/css/menu.css">
 <link rel="stylesheet" href="assets/css/funcionario.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <!-- fim main css -->
 
--- front-end/menu.php
+++ front-end/menu.php
@@ -1,21 +1,46 @@
 <header>
-    <div class="nav01">
-        <h1>Book Solution</h1>    
-        <nav>
-            <ul>
-                <li><a id="a01" href="home" target="blank">Home</a></li>
-
-                <li><a id="a01" href="cadastro_usuario" target="blank">Usuários</a></li>
-    
-                <li><a id="a01" href="cliente">Clientes</a></li>
-
-                <li><a id="a01" href="livros" target="blank">Livros</a></li>
-                
-                <li><a id="a01" href="reserva">Reservas</a></li>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #483D8B;">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white" .text-white href="#">Book Solution Logo</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav teste">
+              <li class="nav-item teste">
+                <a class="nav-link active teste text-white" .text-white aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown text-white" .text-white href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Cadastros
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item text-black" .text-black href="#">Usuários</a></li>
+                  <li><a class="dropdown-item text-black" .text-black href="#">Clientes</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown text-white" .text-white href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Livros
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item" href="#">Consulta</a></li>
+                  <li><a class="dropdown-item" href="#">Cadastro</a></li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown text-white" .text-white href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Reservas
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a class="dropdown-item" href="#">Consulta</a></li>
+                  <li><a class="dropdown-item" href="#">Reserva</a></li>
+                </ul>
+              </li>
             </ul>
-        </nav>
-
-    </div>
+          </div>
+        </div>
+      </nav>
 </header>
 
 
