dfff9133230c77ff701a76487985cfb6ec0bca09
Allana33 <allana.alves08@hotmail.com>
2022-03-21 16:42:27 -0300

    att navbar e form

--- front-end/formulario_usuario.php
+++ front-end/formulario_usuario.php
@@ -1,42 +1,62 @@
 
-    <h1>Cadastro de Usuário</h1> 
-        <div class= "divprincipal">
-            <form method="POST" action="./cadastro_usuario">
-                <div class="teste">
-                    <label for="nomecompleto">Nome</label>
-                    <input type="text" required name="nome_completo_funcionario" placeholder="Digite o nome completo">
-                    <label for="cpf">CPF</label>
-                    <input type="text" required name="cpf_funcionario" placeholder="Digite o cpf sem os pontos">
-                </div>
-
-                <div>
-                <label for="cargo">Cargo</label>
-                <input type="text" required name="cargo" placeholder="ex: professor">
-                <label for="data_admissão">Admissão</label>
-                <input type="date" required name="data_ademissão">
-                </div>
-
-                <div>
-                <label for="telefone">Telefone</label>
-                <input type="tel" required name="telefone" placeholder="(xx) xxxx-xxxx">
-                <label for="celular">Celular</label>
-                <input type="tel" required name="celular" placeholder="(xx) xxxxx-xxxx">
-                </div>
-
-                <div>
-                <label for="email">Email</label>
-                <input type="email" required name="email" placeholder="seuemail@dominio.com">
-                <label for="usuario">Usuário</label>
-                <input type="text" required name="usuario" placeholder="Cadastre um nome para usuário">
-                </div>
-
-                <div>
-                    <label for="senha">Senha</label>
-                    <input type="password" required name="senha" placeholder="Cradastre uma senha">
-                </div>
-
-                <div class="enviar">
-                <button type="submit" value="cadastro_funcionario" name="cadastro_funcionario">Cadastrar</button>
-                </div>
-            </form>
-        </div>
+    <div class="container">
+        <div class="text">Cadastro de Usuários</div>
+        <form method="POST" action="./cadastro_usuario"> 
+           <div class="form-row">
+              <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">Nome Completo</label>
+              </div>
+              <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">CPF</label>
+              </div>
+           </div>
+           <div class="form-row">
+              <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">Email</label>
+              </div>
+              <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">Celular</label>
+              </div>
+           </div>
+           <div class="form-row">
+           <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">Cargo</label>
+              </div>
+              <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">Data de Admissão</label>
+              </div>
+           </div>
+           <div class="form-row">
+           <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">Nome de Usuário</label>
+              </div>
+              <div class="input-data">
+                 <input type="text" required>
+                 <div class="underline"></div>
+                 <label for="">Senha</label>
+              </div>
+           </div>
+                 <div class="form-row submit-btn">
+                    <div class="input-data">
+                       <div class="inner"></div>
+                       <input type="submit" value="Cadastrar">
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </form>
+     </div>
--- front-end/menu.php
+++ front-end/menu.php
@@ -1,33 +1,34 @@
 <header>
-<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #483D8B;">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #625491;">
         <div class="container-fluid">
           <a class="navbar-brand text-white" .text-white href="#">  
             <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-      <img src="assets/img/faviconx.png" alt="" width="40" height="40" class="d-inline-block align-text-top  justify-content-end">
-      Bootstrap
-    </a>
-    <button class="navbar-toggler  justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+      <a class="navbar-brand text-white" .text-white href="#">
+      <img src="assets/img/faviconx.png" alt="" width="40" height="40" class="d-inline-block align-text-top  justify-content-end"> 
+      Book Solution
+    </a>
+    
   </div>
     </a>
           <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav teste">
               <li class="nav-item teste">
-                <a class="nav-link active teste text-white" .text-white aria-current="page" href="home">Home</a>
+                <a class="nav-link active teste text-white btn btn-center" center .text-white aria-current="page" href="home">Home</a>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown text-white" .text-white href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown text-white btn btn-center" center .text-white id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Cadastros
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                   <li><a class="dropdown-item text-black" .text-black href="cadastro_usuario">Usuários</a></li>
-                  <li><a class="dropdown-item text-black" .text-black href="cliente_conteudo">Clientes</a></li>
+                  <li><a class="dropdown-item text-black" .text-black href="cadastro_cliente">Clientes</a></li>
                 </ul>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown text-white" .text-white href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown text-white btn btn-center" center .text-white  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Livros
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -36,12 +37,12 @@
                 </ul>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown text-white" .text-white href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown text-white btn btn-center" center .text-white  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Reservas
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                   <li><a class="dropdown-item" href="#">Consulta</a></li>
-                  <li><a class="dropdown-item" href="#">Reserva</a></li>
+                  <li><a class="dropdown-item" href="reserva">Reserva</a></li>
                 </ul>
               </li>
             </ul>
