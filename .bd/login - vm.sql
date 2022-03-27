ad7da4e426d7f20bb20e0a908e73886f6dc40cdd
lucasmenchon <it.lucas@outlook.com>
2022-03-27 01:17:08 -0300

    att login vm

--- .bd/login - vm.sql
+++
@@ -1,3 +0,0 @@
-USE book_solution;
-
-INSERT INTO funcionario (nome_funcionario,cpf_funcionario, cargo, usuario_funcionario, senha_funcionario,email_funcionario) VALUES ('ALR Developers','12345678910','Desenvolvedores', 'alradmin','','alr@dev.com'); -- senha ALR#1245#
